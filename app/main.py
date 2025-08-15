e275cef726000c9c2525db3a3c138d4239a64506
Victor Macedo <victormcdcosta@gmail.com>
2025-08-15 16:10:06 -0300

    add/ include routes in main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routes import auth
+from app.routes import auth, user, driver, vehicle, schedule
 
-# Criar tabelas
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Sistema de Gestão de Frota")
+
 app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(driver.router)
+app.include_router(vehicle.router)
+app.include_router(schedule.router)
