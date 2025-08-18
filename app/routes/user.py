82d490e4907e785a5ebec41566a9ee381c7d83da
Victor Macedo <victormcdcosta@gmail.com>
2025-08-18 11:55:59 -0300

    remove commented function in routes/user.py

--- app/routes/user.py
+++ app/routes/user.py
@@ -45,17 +45,6 @@ def create_user(user: UserCreate, db: Session = Depends(get_db), current_user = 
 def list_users(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
     return db.query(User).all()
 
-# @router.put("/{user_id}", response_model=UserOut)
-# def update_user(user_id: int, user: UserCreate, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-#     db_user = db.query(User).filter(User.id == user_id).first()
-#     if not db_user:
-#         raise HTTPException(status_code=404, detail="Usuário não encontrado")
-#     for field, value in user.dict().items():
-#         setattr(db_user, field, value)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
-
 @router.put("/{user_id}", response_model=UserOut)
 def update_user(
     user_id: int,
