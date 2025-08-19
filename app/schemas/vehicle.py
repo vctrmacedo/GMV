ac74d3fa38a78c2bab04cfa3d09f45e828706b02
Victor Macedo <victormcdcosta@gmail.com>
2025-08-19 15:14:08 -0300

    fix/ updated the schema for vehicles where the value is boolean

--- app/schemas/vehicle.py
+++ app/schemas/vehicle.py
@@ -1,16 +1,20 @@
 from pydantic import BaseModel
 
+# Modelo base comum
 class VehicleBase(BaseModel):
     license_plate: str
     model: str
     year: int
     current_mileage: int = 0
+    active: bool = True  # Define como booleano desde o início
 
+# Para criação
 class VehicleCreate(VehicleBase):
     pass
 
+# Para saída (GET)
 class VehicleOut(VehicleBase):
     id: int
-    active: bool
+
     class Config:
         from_attributes = True
